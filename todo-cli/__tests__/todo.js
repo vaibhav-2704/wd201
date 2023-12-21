f6e868a7d3fac9f5ec5dbc753823853efbf0ccb7
vaibhav-2704 <vaibhavsaloora27@gmail.com>
2023-12-21 22:52:04 +0530

    Initial commit

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -7,7 +7,7 @@ describe("Todolist", () => {
         const todoItemsCount = todos.all.length;
         todos.add(
             {
-                title: "Todo Test",
+                title: "Todo Testing",
                 completed: false,
                 dueDate: 2023-12-18
             }
@@ -23,7 +23,7 @@ describe("Todolist", () => {
     const dateToday = new Date();
     const formattedDate = (d) => d.toISOString().split('T')[0]
     const yesterday = formattedDate(new Date(dateToday.setDate(dateToday.getDate() - 1)));
-    const od = {title: 'practise Coding', dueDate: yesterday,completed:false};
+    const od = {title: 'overdue', dueDate: yesterday,completed:false};
     const overdueic=todos.overdue().length;
     todos.add(od);
     const overdueItems=todos.overdue();
@@ -34,7 +34,7 @@ describe("Todolist", () => {
     const dateToday = new Date();
     const formattedDate = (d) => d.toISOString().split('T')[0];
     const today = formattedDate(dateToday);
-    const todayAdd={title: 'Do clothes',dueDate: today,completed:false};
+    const todayAdd={title: 'Due Today',dueDate: today,completed:false};
     const duetic=todos.dueToday().length;
     todos.add(todayAdd);
     const todayItems = todos.dueToday();
@@ -45,7 +45,7 @@ describe("Todolist", () => {
     const dateToday = new Date();
     const formattedDate = (d) => d.toISOString().split('T')[0];
     const tomorrow = formattedDate(new Date(dateToday.setDate(dateToday.getDate() + 1)));
-    const dl={title:'Read a book',dueDate:tomorrow,completed:false};
+    const dl={title:'Due later',dueDate:tomorrow,completed:false};
     const duelaterTodoItemsCount =todos.dueLater().length
     todos.add(dl);
     expect(todos.dueLater().length).toBe(duelaterTodoItemsCount+1);
